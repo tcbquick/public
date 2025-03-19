9b14565c3664622c9ea5c443c048d1848635acbb
tcbquick <tcbquick@users.noreply.github.com>
2025-03-19 16:05:12 -0400

    Update and rename #Step000_Install_ansible.sh to Step000_Install_ansible.sh

--- Step000_Install_ansible.sh
+++ Step000_Install_ansible.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "curl -sSL https://raw.githubusercontent.com/tcbquick/public/main/#Step000_Install_ansible.sh | bash"
+echo "curl -sSL https://raw.githubusercontent.com/tcbquick/public/main/Step000_Install_ansible.sh | bash"
 sudo apt update -y && \
 sudo apt install -y software-properties-common && \
 sudo apt install -y ansible && \
