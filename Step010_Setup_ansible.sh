e50dcde76eceafe1f43483eac2c81b1248b6a5d1
tcbquick <tcbquick@users.noreply.github.com>
2025-03-19 22:24:31 +0000

    Update Step010_Setup_ansible.sh

--- Step010_Setup_ansible.sh
+++ Step010_Setup_ansible.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "curl -sSL https://raw.githubusercontent.com/tcbquick/public/main/Step010_Setup_ansible.sh | bash"
 curl -sSL https://raw.githubusercontent.com/tcbquick/public/main/Step010_Setup_ansible.yaml > /home/ansible/Step010_Setup_ansible.yaml
-ansible-playbook -i localhost, /home/ansible/Step010_setup_ansible.yaml
+ansible-playbook -i localhost /home/ansible/Step010_Setup_ansible.yaml
