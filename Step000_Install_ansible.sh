8c0b64b03675b064a106c31d54fbe7194f377649
tcbquick <tcbquick@users.noreply.github.com>
2025-03-19 19:23:06 +0000

    Update Step000_Install_ansible.sh

--- Step000_Install_ansible.sh
+++ Step000_Install_ansible.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "curl -sSL https://raw.githubusercontent.com/tcbquick/home/#Step000_Install_ansible.sh | bash"
+echo "curl -sSL https://raw.githubusercontent.com/tcbquick/public/main/Step000_Install_ansible.sh | bash"
 sudo apt update -y && \
 sudo apt install -y software-properties-common && \
 sudo add-apt-repository --yes --update ppa:ansible/ansible && \
