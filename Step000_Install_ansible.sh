026330fbd916e5969470adc3640f107148210aa9
tcbquick <tcbquick@users.noreply.github.com>
2025-03-19 18:44:29 +0000

    Update Step000_Install_ansible.sh

--- Step000_Install_ansible.sh
+++ Step000_Install_ansible.sh
@@ -4,4 +4,5 @@ sudo apt update -y && \
 sudo apt install -y software-properties-common && \
 sudo add-apt-repository --yes --update ppa:ansible/ansible && \
 sudo apt install -y ansible && \
-ansible --version
+echo "ansible --version"
+
