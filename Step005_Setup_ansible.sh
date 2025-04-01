0bb274da363d601959cda19e2432a85e4bdef13f
tcbquick <tcbquick@users.noreply.github.com>
2025-04-01 08:26:17 -0400

    Create Step005_Setup_ansible.sh

---
+++ Step005_Setup_ansible.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "curl -sSL https://raw.githubusercontent.com/tcbquick/public/main/Step005_Setup_ansible.sh | bash"
+echo "fetch play-book yaml then run it"
+curl -sSL https://raw.githubusercontent.com/tcbquick/public/main/Step005_Setup_ansible.yaml > /home/ansible/Step010_Setup_ansible.yaml
+ansible-playbook -i localhost --become --ask-become-pass /home/ansible/Step005_Setup_ansible.yaml
