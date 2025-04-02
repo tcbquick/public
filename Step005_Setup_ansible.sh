aad2ace6bba03181dd279ab125c111aab02f3ad5
tcbquick <tcbquick@users.noreply.github.com>
2025-04-02 16:56:27 -0400

    Update Step005_Setup_ansible.sh

--- Step005_Setup_ansible.sh
+++ Step005_Setup_ansible.sh
@@ -2,4 +2,5 @@
 echo "curl -sSL https://raw.githubusercontent.com/tcbquick/public/main/Step005_Setup_ansible.sh | bash"
 echo "fetch play-book yaml then run it"
 curl -sSL https://raw.githubusercontent.com/tcbquick/public/main/Step005_Setup_ansible.yaml > /home/ansible/Step005_Setup_ansible.yaml
-ansible-playbook -l prod -i /home/ansible/C1/Invetories/hosts.ini --become --ask-become-pass /home/ansible/Step005_Setup_ansible.yaml
+#ansible-playbook -l prod -i /home/ansible/C1/Invetories/hosts.ini --become --ask-become-pass /home/ansible/Step005_Setup_ansible.yaml
+ansible-playbook -i "127.0.0.1" --become --ask-become-pass /home/ansible/Step005_Setup_ansible.yaml
